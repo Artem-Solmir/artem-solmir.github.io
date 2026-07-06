3699dc55a3fd7a9c220f69c5ef61bf392cf8d20a
Artem Sopot <artem.sopot@solva.kz>
2026-07-06 17:45:17 +0300

    fix: configure for GitHub Pages with HashRouter and homepage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { RootLayout } from './layout';
 import { TicTacToePage } from '../pages';
 
 export const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<RootLayout />} />
         <Route path="/tictactoe" element={<TicTacToePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
